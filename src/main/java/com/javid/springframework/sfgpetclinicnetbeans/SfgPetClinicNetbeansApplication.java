c7072aa4e456490853ec429aa459a0486646f381
Javid Rafiee <javid.rafiee@gmail.com>
2020-12-21 18:42:22 +0330

    Initial Project Add

---
+++ src/main/java/com/javid/springframework/sfgpetclinicnetbeans/SfgPetClinicNetbeansApplication.java
@@ -0,0 +1,13 @@
+package com.javid.springframework.sfgpetclinicnetbeans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinicNetbeansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgPetClinicNetbeansApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javid/springframework/sfgpetclinicnetbeans/SfgPetClinicNetbeansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javid.springframework.sfgpetclinicnetbeans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicNetbeansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
